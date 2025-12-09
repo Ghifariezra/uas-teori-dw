407ede53e8fa8c8762921e307cefc2da5df91e0a
mfrhlisa <lisabiliana13@gmail.com>
2025-12-09 22:53:45 +0700

    add: fitur kategori

---
+++ scripts/sections/kategori.js
@@ -0,0 +1,9 @@
+export default async function renderKategori(){
+    const sectionContent = document.getElementById("content");
+    if (!sectionContent) return;
+
+    const res = await fetch("/components/kategori.html");
+    const html = await res.text();
+
+    sectionContent.innerHTML = html;
+}
