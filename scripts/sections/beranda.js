f2722c10bf984e5fb09529a489a03fe9385c6b7c
Ghifariezra <ghifariezraramadhan@gmail.com>
2025-12-09 17:05:56 +0700

    update: carousel

---
+++ scripts/sections/beranda.js
@@ -0,0 +1,9 @@
+export default async function renderBeranda(){
+    const sectionContent = document.getElementById("content");
+    if (!sectionContent) return;
+
+    const res = await fetch("/components/beranda.html");
+    const html = await res.text();
+
+    sectionContent.innerHTML = html;
+}
