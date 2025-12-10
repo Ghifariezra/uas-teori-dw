b9a95f9f61b676a83f76b5e21a69f93af1240eab
Ghifariezra <ghifariezraramadhan@gmail.com>
2025-12-10 11:18:22 +0700

    fix github pages

--- scripts/utils/load.js
+++ scripts/utils/load.js
@@ -1,4 +1,10 @@
 export async function loadComponents(path) {
-    const res = await fetch(`/components/${path}`);
+    const isGithub = window.location.hostname.includes("github.io");
+
+    const BASE = isGithub
+        ? `${window.location.origin}/uas-teori-dw`
+        : window.location.origin;
+
+    const res = await fetch(`${BASE}/components/${path}`);
     return await res.text();
 }
