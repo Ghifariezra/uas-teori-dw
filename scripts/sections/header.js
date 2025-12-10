151cdf39639b17a560e8a66d8858a13abf87fed4
Ghifariezra <ghifariezraramadhan@gmail.com>
2025-12-10 17:47:19 +0700

    fix: header

--- scripts/sections/header.js
+++ scripts/sections/header.js
@@ -1,7 +1,7 @@
 import { initToggleMenu } from "../toggle.js";
 import { loadComponents } from "../utils/load.js";
 import { initSearchToggle, initSearchInput } from "./search.js";
-
+ 
 export default async function renderHeader() {
     const headerEl = document.getElementById("header");
     if (!headerEl) return;
