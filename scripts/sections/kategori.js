4fd803add9a41a14ff415c9b4bb905da9b99a906
Ghifariezra <ghifariezraramadhan@gmail.com>
2025-12-10 10:15:41 +0700

    fix github pages

--- scripts/sections/kategori.js
+++ scripts/sections/kategori.js
@@ -2,7 +2,7 @@ export default async function renderKategori(){
     const sectionContent = document.getElementById("content");
     if (!sectionContent) return;
 
-    const res = await fetch("uas-teori-dw/components/kategori.html");
+    const res = await fetch("../../components/kategori.html");
     const html = await res.text();
 
     sectionContent.innerHTML = html;
