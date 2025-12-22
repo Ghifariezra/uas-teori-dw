ddee4b8df2127e9395024eaa00ca67f02c068e81
Owner <ghifariezraramadhan@gmail.com>
2025-12-22 12:25:06 +0700

    update: src image carousel

--- scripts/data/carouselData.js
+++ scripts/data/carouselData.js
@@ -1,17 +1,17 @@
 const carouselData = [
   {
     type: "video",
-    src: "/assets/carousel/carousel1.mp4",
+    src: `${window.location.pathname}/assets/carousel/carousel1.mp4`,
     alt: "Whispers of the Past"
   },
   {
     type: "image",
-    src: "/assets/carousel/carousel2.png",
+    src: `${window.location.pathname}/assets/carousel/carousel2.png`,
     alt: "Step Back in Time"
   },
   {
     type: "image",
-    src: "/assets/carousel/carousel3.png",
+    src: `${window.location.pathname}/assets/carousel/carousel3.png`,
     alt: "Timeless Collections"
   }
 ];
