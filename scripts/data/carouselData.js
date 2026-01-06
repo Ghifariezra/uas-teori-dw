09c75449cf406bff1edc2ba3fe361d5f80d5aeba
Owner <ghifariezraramadhan@gmail.com>
2026-01-06 08:58:06 +0700

    fix: bug

--- scripts/data/carouselData.js
+++ scripts/data/carouselData.js
@@ -1,17 +1,17 @@
 const carouselData = [
   {
     type: "video",
-    src: `${window.location.pathname}/assets/carousel/carousel1.mp4`,
+    src: "/uas-teori-dw/assets/carousel/carousel1.mp4",
     alt: "Whispers of the Past"
   },
   {
     type: "image",
-    src: `${window.location.pathname}/assets/carousel/carousel2.png`,
+    src: "/uas-teori-dw/assets/carousel/carousel2.png",
     alt: "Step Back in Time"
   },
   {
     type: "image",
-    src: `${window.location.pathname}/assets/carousel/carousel3.png`,
+    src: "/uas-teori-dw/assets/carousel/carousel3.png",
     alt: "Timeless Collections"
   }
 ];
